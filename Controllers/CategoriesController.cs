6e4e995f78d96a6397e784cd1d0646b7cb2801e9
Rating001 <106230549+Rating001@users.noreply.github.com>
2022-12-17 12:50:28 -0800

    Deleted the "Details" action in the categories controller and the "Details" view. Video 50 of 60.

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -93,25 +93,7 @@ namespace ContactPro.Controllers
             return View(ecvm);
         }
 
-        [Authorize]
-        // GET: Categories/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
 
-            var category = await _context.Categories
-                .Include(c => c.AppUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
         [Authorize]
         // GET: Categories/Create
         public IActionResult Create()
