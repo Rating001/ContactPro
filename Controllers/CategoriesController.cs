ec64bbd669ad09da6f98624abf9c0f2a33930f2a
Rating001 <106230549+Rating001@users.noreply.github.com>
2022-12-17 11:29:17 -0800

    Added Sweet Alerts fo the "Group/Category Email" page. Video 48 of 60.

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -10,6 +10,7 @@ using ContactPro.Data;
 using ContactPro.Models;
 using Microsoft.AspNetCore.Authorization;
 using ContactPro.Models.ViewModels;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace ContactPro.Controllers
 {
@@ -17,17 +18,22 @@ namespace ContactPro.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<AppUser> _userManager;
+        private readonly IEmailSender _emailService;
 
         public CategoriesController(ApplicationDbContext context,
-                                    UserManager<AppUser> userManager)
+                                    UserManager<AppUser> userManager,
+                                    IEmailSender emailService)
         {
             _context = context;
             _userManager = userManager;
+            _emailService = emailService;
         }
         [Authorize]
         // GET: Categories
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string swalMessage = null)
         {
+            ViewData["SwalMessage"] = swalMessage;
+
             string appUserId = _userManager.GetUserId(User);
 
 
@@ -65,6 +71,26 @@ namespace ContactPro.Controllers
             };
 
             return View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> EmailCategory(EmailCategoryViewModel ecvm)
+        {
+            if (ModelState.IsValid)
+            {
+                try 
+                { 
+                    await _emailService.SendEmailAsync(ecvm.EmailData.EmailAddress,ecvm.EmailData.Subject,ecvm.EmailData.Body);
+                    return RedirectToAction("Index", "Categories", new {swalMessage = "Success: Email Sent!"});
+                }
+                catch
+                {
+                    return RedirectToAction("Index", "Categories", new { swalMessage = "Error: Email Send Failed!" });
+                    throw;
+                }
+            }
+            return View(ecvm);
         }
 
         [Authorize]
